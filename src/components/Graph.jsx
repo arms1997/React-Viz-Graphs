95c293d5df52e63adc4b57f8da9b9f779fe3011d
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-13 21:18:29 -0500

    data move here

--- src/components/Graph.jsx
+++ src/components/Graph.jsx
@@ -1,5 +1,18 @@
 import LineAreaBubble from "./LineAreaBubble";
 
+import { parser, xAxisParser } from "../samples/parser";
+import DATA from "../samples/alpha_vantage";
+
 export default function Graph(props) {
-  return <div>{props.lineWithBubble && <LineAreaBubble />}</div>;
+  const alphaData = parser({ ...DATA });
+
+  const areaData = alphaData.filter((data) => data.x > 50 && data.x < 64);
+
+  return (
+    <div>
+      {props.lineWithBubble && (
+        <LineAreaBubble areaData={areaData} alphaData={alphaData} />
+      )}
+    </div>
+  );
 }
