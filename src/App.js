6f5fcaa92ce68b5a847d5ad8849b57d1a6592fd5
Armeen Hadizadeh <armeen.hadizadeh@gmail.com>
2021-03-14 12:22:19 -0400

    react router implementation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import "./App.css";
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
 //Graph Import
 import Graph from "./components/Graph";
 
 function App() {
   return (
     <div className="App">
-      <Graph lineWithBubble />
+      <Router>
+        <Switch>
+          <Route path="/lineWithBubble">
+            <Graph lineWithBubble />
+          </Route>
+          <Route path="/lineWithDynamicBubble">
+            <Graph lineWithDynamicBubble />
+          </Route>
+          <Route path="/">
+            <Graph lineWithBubble />
+            <Graph lineWithDynamicBubble />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
